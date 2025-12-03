3293cb215cbf6dd533185434355e1f02cc4971ea
Wynadorn <janosdevries@gmail.com>
2025-12-03 16:35:30 +0100

    3-2

--- solver/Solvers/Day3.cs
+++ solver/Solvers/Day3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AoC2025.Solvers
@@ -22,9 +23,18 @@ namespace AoC2025.Solvers
 
         public string SolvePartTwo(string puzzleInput, string[] puzzleInputArray)
         {
-            
+            double maxJoltage = 0;
 
-            return "";
+            foreach(string bankString in puzzleInputArray)
+            {
+                //Console.WriteLine($"  Processing: {bankString}");
+                Bank bank = new Bank(bankString);
+                double joltage = bank.GetUnsafeBankJoltage();
+                maxJoltage += joltage;
+                //Console.WriteLine($"    {bankString} = {joltage}");
+            }
+
+            return $"{maxJoltage}";
         }
     }
 
@@ -42,10 +52,12 @@ namespace AoC2025.Solvers
 
     public class Bank
     {
+        string SerializedBank;
         public Battery[] Batteries { get; set;}
 
         public Bank(string serializedBank)
         {
+            SerializedBank = serializedBank;
             Batteries = new Battery[serializedBank.Length];
             
             for(int i = 0; i<serializedBank.Length; i++)
@@ -58,6 +70,37 @@ namespace AoC2025.Solvers
             var b = Batteries.Skip(a.Index+1).Max(b => b.Joltage);
 
             return a.Joltage * 10 + b;
+        }
+
+        public double GetUnsafeBankJoltage(int batteriesPerBank = 12)
+        {
+            double joltage=0;
+            int skip = 0;
+            for(int i = 1; i <= batteriesPerBank; i++)
+            {
+                Battery a = Batteries.Skip(skip).SkipLast(batteriesPerBank-i).MaxBy(b => b.Joltage);
+                skip = a.Index +1;
+                double d = a.Joltage * Math.Pow(10, batteriesPerBank-i);
+                //Console.WriteLine($"      {{0,{11+batteriesPerBank}}}", $"{d}");
+                joltage += d;
+            }
+            
+            // Battery b = Batteries.SkipLast(batteriesPerBank-2).MaxBy(b => b.Joltage);
+            // Battery c = Batteries.SkipLast(batteriesPerBank-3).MaxBy(b => b.Joltage);
+
+            //List<long> joltages = new List<long>();
+
+            // int iStart = 0;
+            // while(iStart+batteriesPerBank <= SerializedBank.Length)
+            // {
+            //     string sub = SerializedBank[iStart..(iStart+batteriesPerBank)];
+            //     long intValue = Convert.ToInt64(sub);
+            //     joltages.Add(intValue);
+            //     //Console.WriteLine($"        sub[{iStart}..{iStart+batteriesPerBank}] is {sub}");
+            //     iStart++;
+            // }
+
+            return joltage;
         }
     }
 }
